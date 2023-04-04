2ff83e44ab284e43f88e6bcf42c49fb3c4ff49c5
omkar-mohanty <franzohouser@gmail.com>
2023-04-04 20:58:51 +0530

    lib: Add AST lexer

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
 mod lexer;
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub enum Expr {
+    Literal(String),
+    Identifier(String),
+    Assign(String, Box<Expr>),
+    GlobalDataAddr(String),
+}
