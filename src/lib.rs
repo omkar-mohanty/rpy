63ba1757403f04553ac926e4979bb7ee456baa4a
omkar-mohanty <franzohouser@gmail.com>
2023-04-04 20:59:52 +0530

    lib: add assignment and literal parser

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,24 @@ pub enum Expr {
     Assign(String, Box<Expr>),
     GlobalDataAddr(String),
 }
+
+peg::parser! {pub grammar parser() for str {
+    pub rule expression() -> Expr
+        = assignment() /
+          literal()
+
+    rule assignment() -> Expr
+        = i:identifier() _ "=" _ e:expression() {Expr::Assign(i, Box::new(e))}
+
+    rule identifier() -> String
+        = quiet!{ n:$(['a'..='z' | 'A'..='Z' | '_']['a'..='z' | 'A'..='Z' | '0'..='9' | '_']*) { n.to_owned() } }
+        / expected!("identifier")
+
+   rule literal() -> Expr
+        = n:$(['0'..='9']+) { Expr::Literal(n.to_owned()) } /
+            i:identifier() {Expr::GlobalDataAddr(i)}
+
+     rule _() =  quiet!{[' ' | '\t']*}
+}}
+
+
