869fe963e3751bfec81bd12c8f634dab40f90165
omkar-mohanty <franzohouser@gmail.com>
2023-04-05 12:33:38 +0530

    main: parse from input

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 
 use clap::Parser;
 use rpy::Result;
+use rpy::parser as rpy_parser;            
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -10,5 +11,19 @@ struct Args {
 }
 
 fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let source = std::fs::read_to_string(args.source_file)?;
+
+    println!("{}", source);
+
+    let ast = rpy_parser::statements(&source)?;
+
+    println!("{}", ast.len());
+
+    for node in ast {
+        println!("{}", node)
+    }
+
     Ok(())
 }
