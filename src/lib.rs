2f29b08d29db3adbd414193cc06d8494d9e28f8a
omkar-mohanty <franzohouser@gmail.com>
2023-04-06 22:34:27 +0530

    lib: add new expression parser

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,9 @@ peg::parser! {pub grammar parser() for str {
 
     rule simple_statement() -> Expr = assignment()
 
-    rule assignment() -> Expr = [' ' | '\t' | '\n']* id:name() _ "=" expr:expression() "\n"* { Expr::Assign(id, expr)}
+    rule assignment() -> Expr = [' ' | '\t' | '\n']* id:name() _ "=" _ expr:expression() "\n"* { Expr::Assign(id, Box::new(expr))}
+
+    rule expression() -> Expr = assignment() 
 
     rule name() -> String
         = quiet!{ n:$(['a'..='z' | 'A'..='Z' | '_']['a'..='z' | 'A'..='Z' | '0'..='9' | '_']*) { n.to_owned() } }
