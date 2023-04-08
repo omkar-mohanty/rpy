70076b5d5d4b42201a771328f1b31ea3c3da14d7
omkar-mohanty <franzohouser@gmail.com>
2023-04-08 21:09:16 +0530

    jit: implement default

--- src/jit.rs
+++ src/jit.rs
@@ -1,10 +1,34 @@
 use cranelift::prelude::*;
 use cranelift_jit::{JITModule, JITBuilder};
-use cranelift_module::DataContext;
+use cranelift_module::{DataContext, Module};
 
-struct JIT {
+pub struct JIT {
     builder_context: FunctionBuilderContext, 
     ctx: codegen::Context,
     data_ctx: DataContext,
     module: JITModule,
 }
+
+impl Default for JIT {
+    fn default() -> Self {
+        let mut flag_builder = settings::builder();
+        flag_builder.set("use_colocated_libcalls", "false").unwrap();
+        flag_builder.set("is_pic", "false").unwrap();
+        let isa_builder = cranelift_native::builder().unwrap_or_else(|msg| {
+            panic!("host machine is not supported: {}", msg);
+        });
+        let isa = isa_builder
+            .finish(settings::Flags::new(flag_builder))
+            .unwrap();
+        let builder = JITBuilder::with_isa(isa, cranelift_module::default_libcall_names());
+
+        let module = JITModule::new(builder);
+        Self {
+            builder_context: FunctionBuilderContext::new(),
+            ctx: module.make_context(),
+            data_ctx: DataContext::new(),
+            module,
+        }
+    }
+}
+
