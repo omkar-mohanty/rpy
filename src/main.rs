ed86dc57cfd79e8d206d5dec6c7f12d0069503e0
omkar-mohanty <franzohouser@gmail.com>
2023-04-14 20:52:37 +0530

    main: run code ptr

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use core::mem;
 
 use clap::Parser;
 use rpy::{Result, Sesssion};
@@ -14,7 +15,21 @@ fn main() -> Result<()> {
 
     let source = std::fs::read_to_string(args.source_file)?;
 
-    let session = Sesssion::new(source, args.output_file);
+    unsafe {run_code::<(), ()>(source, (), args.output_file)?};
+    
 
     Ok(())
+}
+
+unsafe fn run_code<I, O> (source: String, input: I, output_file: Option<PathBuf>) -> Result<O> {
+    let session = Sesssion::new(source, output_file);
+
+// Pass the string to the JIT, and it returns a raw pointer to machine code.
+    let code_ptr = session.compile()?;
+    // Cast the raw pointer to a typed function pointer. This is unsafe, because
+    // this is the critical point where you have to trust that the generated code
+    // is safe to be called.
+    let code_fn = mem::transmute::<_, fn(I) -> O>(code_ptr);
+    // And now we can call it!
+    Ok(code_fn(input))
 }
